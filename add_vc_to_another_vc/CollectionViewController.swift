d0f200b5122047952f10f1dfebbe849b2e3294df
aPuYue <pu.yue@access-company.com>
2022-06-30 14:57:39 +0900

    成功

---
+++ add_vc_to_another_vc/CollectionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionViewController.swift
+//  add_vc_to_another_vc
+//
+//  Created by Pu Yue - PU YUE on 2022/06/30.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+
+    }
+
+
+}
--- add_vc_to_another_vc/ViewController.swift
+++ add_vc_to_another_vc/ViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var contentView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let vc = storyboard!.instantiateViewController(withIdentifier: "CollectionViewController")
+        self.addChild(vc)
+        vc.view.frame = contentView.bounds  // or, better, turn off `translatesAutoresizingMaskIntoConstraints` and then define constraints for this subview
+        contentView.addSubview(vc.view)
+//        vc.didMove(toParent: self)
+        
     }
 
 
