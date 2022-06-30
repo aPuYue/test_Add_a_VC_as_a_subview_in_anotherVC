8b6063cea0b30e906a20d02a5a955bbba21a57f3
aPuYue <pu.yue@access-company.com>
2022-06-30 14:45:34 +0900

    Initial Commit

---
+++ add_vc_to_another_vc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  add_vc_to_another_vc
+//
+//  Created by Pu Yue - PU YUE on 2022/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
