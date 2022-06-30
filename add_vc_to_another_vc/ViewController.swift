39cfa5d99e3c8368abbd5337d54362b182b5e0b8
aPuYue <pu.yue@access-company.com>
2022-06-30 15:22:11 +0900

    Add memo

--- add_vc_to_another_vc/ViewController.swift
+++ add_vc_to_another_vc/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         
         let vc = storyboard!.instantiateViewController(withIdentifier: "CollectionViewController")
         self.addChild(vc)
-        vc.view.frame = contentView.bounds  // or, better, turn off `translatesAutoresizingMaskIntoConstraints` and then define constraints for this subview
+        vc.view.frame = contentView.bounds//外部view的内size赋值给内部view的外size  // or, better, turn off `translatesAutoresizingMaskIntoConstraints` and then define constraints for this subview
         contentView.addSubview(vc.view)
         vc.didMove(toParent: self)
         //For more information about why this addChild (previously called addChildViewController) and didMove(toParent:) (previously called didMove(toParentViewController:)) are necessary, see WWDC 2011 video #102 - Implementing UIViewController Containment. In short, you need to ensure that your view controller hierarchy stays in sync with your view hierarchy, and these calls to addChild and didMove(toParent:) ensure this is the case.
