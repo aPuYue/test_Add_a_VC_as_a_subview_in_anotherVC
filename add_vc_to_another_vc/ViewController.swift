c2247ea8e8654a8608e5183004dfbd257f034a82
aPuYue <pu.yue@access-company.com>
2022-06-30 15:13:38 +0900

    简单方法：使用Container View

--- add_vc_to_another_vc/ViewController.swift
+++ add_vc_to_another_vc/ViewController.swift
@@ -15,15 +15,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let vc = storyboard!.instantiateViewController(withIdentifier: "CollectionViewController")
-        self.addChild(vc)
-        vc.view.frame = contentView.bounds  // or, better, turn off `translatesAutoresizingMaskIntoConstraints` and then define constraints for this subview
-        contentView.addSubview(vc.view)
-        vc.didMove(toParent: self)
-        //For more information about why this addChild (previously called addChildViewController) and didMove(toParent:) (previously called didMove(toParentViewController:)) are necessary, see WWDC 2011 video #102 - Implementing UIViewController Containment. In short, you need to ensure that your view controller hierarchy stays in sync with your view hierarchy, and these calls to addChild and didMove(toParent:) ensure this is the case.
-        
-        // Also see Creating Custom Container View Controllers in the View Controller Programming Guide.
-        
+      
     }
 
 
